e563e4ce11e505299eafc97782253c769397ad16
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2020-10-02 17:02:13 +0200

    Add language param to route and load regex from config

--- ms365_accountcreator/routes.py
+++ ms365_accountcreator/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import render_template, request
+from werkzeug.datastructures import LanguageAccept
 from . import APP
 
 from . import api
@@ -7,7 +8,13 @@ if APP.config.get('DEBUG', False):
     from . import debug_routes
 
 
+@APP.route('/', defaults={'lang': ''})
+@APP.route('/<string:lang>')
+def default_route(lang):
+    if lang:
+        # inject language from url as first choice into request
+        values = (lang, 1), *request.accept_languages
+        request.accept_languages = LanguageAccept(values)
+    email_regex = APP.config.get('EMAIL_ADDRESS_FILTER', '.*')
+    return render_template('index.html', email_regex=email_regex)
 
-@APP.route('/')
-def default_route():
-    return render_template('index.html', title='muse4music')
